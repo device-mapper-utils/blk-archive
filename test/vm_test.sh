d783a709a27a34d421784039784b5032572869cc
Tony Asleson <tasleson@redhat.com>
2025-05-30 12:21:00 -0500

    [ci] make test re-runnable

--- test/vm_test.sh
+++ test/vm_test.sh
@@ -19,8 +19,10 @@ apt-get update -y || exit 1
 
 apt-get install git gcc clang-tools libdevmapper-dev pkg-config mount python3 python3-toml python3-pudb python3-numpy "$K_PKG_NAME"  -y || exit 1
 
-# install rust via rustup as packages are too old on ubuntu
-curl https://sh.rustup.rs -sSf | sh -s -- -y || exit 1
+if [ ! -e "$HOME/.cargo/env" ]; then
+    # install rust via rustup as packages are too old on ubuntu
+    curl https://sh.rustup.rs -sSf | sh -s -- -y || exit 1
+fi
 
 # Get rust tools in path
 source "$HOME/.cargo/env" || exit 1
@@ -35,16 +37,28 @@ if [ ! -d dmtest-python ]; then
     git clone https://github.com/jthornber/dmtest-python.git || exit 1
 fi
 
-# Create the block devices
-echo "Creating sparse block images"
-truncate -s 1T /block1.img || exit 1
-truncate -s 1T /block2.img || exit 1
-truncate -s 1T /block3.img || exit 1
 
-echo "Setting up loop back devices ..."
-loop1=$(losetup -f --show /block1.img)
-loop2=$(losetup -f --show /block2.img)
-loop3=$(losetup -f --show /block3.img)
+# Create the block devices and loop back devices if needed
+if [ ! -e /block1.img ]; then
+    echo "Creating sparse block images"
+    truncate -s 1T /block1.img || exit 1
+    truncate -s 1T /block2.img || exit 1
+    truncate -s 1T /block3.img || exit 1
+
+    echo "Setting up loop back devices ..."
+    loop1=$(losetup -f --show /block1.img)
+    loop2=$(losetup -f --show /block2.img)
+    loop3=$(losetup -f --show /block3.img)
+
+    cd "$START_DIR" || exit 1
+    # setup the configuration file for dmtest-python
+    cd dmtest-python || exit 1
+
+    echo "metadata_dev = '$loop1'" > config.toml
+    echo "data_dev = '$loop2'" >> config.toml
+    echo "disable_by_id_check = true" >> config.toml
+fi
+
 
 # Run the cargo based tests
 cd "$START_DIR" || exit 1
@@ -52,17 +66,10 @@ cd "$START_DIR" || exit 1
 echo "Running the cargo tests ..."
 cargo test -- --test-threads=1 || exit 1
 
-# Run the dmtest-python tests for blk-archive
 # Unable to run rolling linux test as we don't have enough disk space in the CI VMs.
-cd "$START_DIR" || exit 1
-# setup the configuration file for dmtest-python
-cd dmtest-python || exit 1
-
-echo "metadata_dev = '$loop1'" > config.toml
-echo "data_dev = '$loop2'" >> config.toml
-echo "disable_by_id_check = true" >> config.toml
-
+# Run the dmtest-python tests for blk-archive
 echo "Running the dmtest combinations..."
+cd dmtest-python || exit 1
 
 export DMTEST_RESULT_SET=unit-test
 ./dmtest health || exit 1
